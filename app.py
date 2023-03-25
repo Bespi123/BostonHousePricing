b9694b0d816949eded6cc715d0d28b4b0a870e83
Bespi123 <b_antonioesp@hotmail.com>
2023-03-24 23:42:18 -0500

    formulario agregado

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ regmodel=pickle.load(open('regmodel.pkl','rb'))
 scalar=pickle.load(open('scaling.pkl','rb'))
 @app.route('/')
 def home():
- return render_template('home.html')
+ return render_template('home2.html')
 @app.route('/predict_api',methods=['POST'])
 def predict_api():
  data=request.json['data']
@@ -20,5 +20,21 @@ def predict_api():
  output=regmodel.predict(new_data)
  print(output[0])
  return jsonify(output[0])
+
+
+@app.route('/predict',methods=['POST'])
+
+def predict():
+
+   data=[float(x) for x in request.form.values()]
+
+   final_input=scalar.transform(np.array(data).reshape(1,-1))
+
+   print(final_input)
+
+   output=regmodel.predict(final_input)[0]
+
+   return render_template("home2.html",prediction_text="The House price prediction is {}".format(output))
+
 if __name__=="__main__":
  app.run(debug=True)
